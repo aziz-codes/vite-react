fc2b27e0be514c44d397ca443bef575199da3f77
aziz-codes <azizisoffline@gmail.com>
2023-01-16 20:09:10 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h3>Welcome to Vite React APP</h3>
+  )
+}
+
+export default App
